03f63e6b6ab3275359b4704920d998e66ca4710b
dhanushranga1 <gdr20704@gmail.com>
2025-12-13 01:20:32 +0530

    add startup hook for db indexes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from app.routers import org, auth
+from app.db import create_indexes
 
 app = FastAPI(
     title="Organization Management Service",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+async def startup():
+    await create_indexes()
 
 app.include_router(org.router, tags=["Organization"])
 app.include_router(auth.router, tags=["Admin"])
