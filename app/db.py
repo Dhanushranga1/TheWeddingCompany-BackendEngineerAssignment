631551c018acf2e7a80e56d8f4f193b2800f5bd6
dhanushranga1 <gdr20704@gmail.com>
2025-12-13 01:11:01 +0530

    add unique indexes for org and admin

--- app/db.py
+++ app/db.py
@@ -7,5 +7,9 @@ db = client[DB_NAME]
 organizations = db["organizations"]
 admins = db["admins"]
 
+async def create_indexes():
+    await organizations.create_index("name", unique=True)
+    await admins.create_index("email", unique=True)
+
 def get_dynamic_collection(name: str):
     return db[name]
